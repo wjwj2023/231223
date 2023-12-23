3e4a1e329f0043f87a4a356616a58348513217a8
dnwlr <dnwlr@DESKTOP-L8GO68Q>
2023-12-23 15:10:44 +0900

    A 수정

--- MyGit/src/hwgit/A.java
+++ MyGit/src/hwgit/A.java
@@ -4,8 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Hello HwGit");
-		B n1 = new B();
-		n1.sayHello("qwe");
+		System.out.println("devB A change in master branch");
 	}
 
 }
--- MyGit/src/hwgit/B.java
+++ MyGit/src/hwgit/B.java
@@ -4,7 +4,7 @@ public class B {
 
 	public void sayHello(String toWhom) {
 		System.out.println("Hello! "+ toWhom);
-
+		System.out.println("devB B change in master branch");
 	}
 
 }
